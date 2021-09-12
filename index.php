4f8ca31d9caf6e847b150800f98cf1c45301fa5b
AJE <77433033+andhikajuliawan@users.noreply.github.com>
2021-09-12 13:01:28 +0700

    menambahakan paragraph di file index.php

--- index.php
+++ index.php
@@ -10,6 +10,6 @@
     
     <h1>Hello world</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur est architecto facilis quae distinctio voluptate reiciendis autem excepturi ullam fugiat id, eos dolorem voluptates pariatur natus dolor, itaque possimus quas amet atque tempora eius officiis! Aliquid dolor alias rerum nemo? Voluptatem facere quia fuga quas dolores! Maxime pariatur cumque, officia atque aspernatur, quibusdam in eum aliquam, illo perferendis commodi. Vero cum dicta sed illum et, facere dolorum praesentium sit expedita blanditiis distinctio quidem magnam a ex, optio placeat necessitatibus inventore accusamus, nesciunt provident voluptas? Tempora, commodi corporis eos fuga ullam quo voluptate eligendi, id laudantium et ipsam, earum aut. Laborum!</p>
-
+    <p>lorem100<p>
 </body>
 </html>
